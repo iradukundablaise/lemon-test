7b7019febb578ec3fe7b0bd8158f866b903e298a
Blaise IRADUKUNDA <ba.iradukunda@gmail.com>
2025-08-28 16:55:50 +0200

    Add method to fetch movies by their id

--- src/Service/TMDbService.php
+++ src/Service/TMDbService.php
@@ -14,6 +14,17 @@ class TMDbService
         $this->searchLoggerService = new SearchLoggerService();
     }
 
+    public function getMovieById(int $id): array {
+        $req = $this->httpClient->get($_ENV["TMDB_API_URL"] . "movie/" . $id . "?language=fr-FR", [
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . $_ENV["TMDB_API_TOKEN"],
+            ],
+        ]);
+
+        return json_decode($req->getBody()->getContents(), true);
+    }
+
     public function getCurrentTopMovies(): array {
         $req = $this->httpClient->get($_ENV["TMDB_API_URL"] . "movie/now_playing?language=fr-FR&page=1", [
             'headers' => [
@@ -24,6 +35,7 @@ class TMDbService
 
         return json_decode($req->getBody()->getContents(), true);
     }
+
     public function searchMovie(string $query, int $page = 1): array {
         if(trim($query) !== ""){
             $this->searchLoggerService->log($query);
