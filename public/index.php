8f7fe192daf17f6cfbb7dc89a16be88eb817f71a
Blaise IRADUKUNDA <ba.iradukunda@gmail.com>
2025-08-28 14:32:42 +0200

    Add application entrypoint php file

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    use Dotenv\Dotenv;
+    use App\Base\Router;
+    use App\Controller\IndexController;
+
+
+    $dotenv = Dotenv::createImmutable(__DIR__."/../");
+    $dotenv->load();
+
+    $router = new Router();
+
+    $router->get("/", [IndexController::class, "index"]);
+    $router->get("/search", [IndexController::class, "search"]);
+
+    try {
+        $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+    }catch (\Exception $e){
+        echo $e->getMessage();
+    }
