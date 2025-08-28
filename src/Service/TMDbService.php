ff2a27516a5ff56881d7dbacd5afb6f7763a66c0
Blaise IRADUKUNDA <ba.iradukunda@gmail.com>
2025-08-28 16:14:28 +0200

    Add currrent top movies method

--- src/Service/TMDbService.php
+++ src/Service/TMDbService.php
@@ -13,6 +13,17 @@ class TMDbService
         $this->httpClient = new Client();
         $this->searchLoggerService = new SearchLoggerService();
     }
+
+    public function getCurrentTopMovies(): array {
+        $req = $this->httpClient->get($_ENV["TMDB_API_URL"] . "movie/now_playing?language=fr-FR&page=1", [
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . $_ENV["TMDB_API_TOKEN"],
+            ],
+        ]);
+
+        return json_decode($req->getBody()->getContents(), true);
+    }
     public function searchMovie(string $query, int $page = 1): array {
         if(trim($query) !== ""){
             $this->searchLoggerService->log($query);
